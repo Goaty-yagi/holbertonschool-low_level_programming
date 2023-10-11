a9e6d5bd90b1c6bfd91defd55a4d35e7adfc1343
Goaty-yagi <tw200blue@gmail.com>
2023-10-10 22:19:27 -0700

    add 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,2 +1,31 @@
 #include <stdio.h>
 
+/**
+ * main - print the size of various types
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char charType;
+
+	int intType;
+
+	long long int longLongIntType;
+
+	float floatType;
+
+	printf("Size of a char:%zu", sizeof(charType));
+
+	printf("Size of a int:%zu", sizeof(intType));
+
+	printf("Size of a long int:%zu", sizeof(intType));
+
+	printf("Size of a long long int:%zu", sizeof(longLongIntType));
+
+	printf("Size of a float:%zu", sizeof(floatType));
+
+	return (0);
+
+
+}
