820426f7ad850564869abb4d194743d74b889ca3
Goaty-yagi <tw200blue@gmail.com>
2023-10-10 21:36:04 -0700

    add betty style and documents to file5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - Print text
+ * 
+ * Return: Always 0 (Success)
+ */
+
 int main() {
 	char text[] = "with proper grammar, but the outcome is a piece of art,";
+
 	printf("%s\n",text);
-	return 0;
+
+	return (0);
 }
