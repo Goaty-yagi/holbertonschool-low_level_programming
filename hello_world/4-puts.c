66e6a71c16a4e9a3ba9552dae55ce3a73c2fa578
Goaty-yagi <tw200blue@gmail.com>
2023-10-10 19:21:13 -0700

    test 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 int main(){
-	char text[100];
-	text = "\"Programming is like building a multilingual puzzle";
+	char text[] = "\"Programming is like building a multilingual puzzle";
 	puts(text);
 	return 0;
