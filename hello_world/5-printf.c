66508b1f2da187ec83530344f100373769de1c38
Goaty-yagi <tw200blue@gmail.com>
2023-10-10 21:39:25 -0700

    fix betty style

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -2,14 +2,14 @@
 
 /**
  * main - Print text
- * 
+ *
  * Return: Always 0 (Success)
  */
-
-int main() {
+int main(void)
+{
 	char text[] = "with proper grammar, but the outcome is a piece of art,";
 
-	printf("%s\n",text);
+	printf("%s\n", text);
 
 	return (0);
 }
