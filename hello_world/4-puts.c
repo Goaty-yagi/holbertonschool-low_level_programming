a6b1e542fbe9639c441f62ec13b95544f3c5df41
Goaty-yagi <tw200blue@gmail.com>
2023-10-10 21:24:46 -0700

    add Betty documentation style

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - Print text
+ *  
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	char text[] = "\"Programming is like building a multilingual puzzle";
