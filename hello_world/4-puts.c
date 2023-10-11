546b3043908a5228529cc7eeaa999aed3e8a8b1a
Goaty-yagi <tw200blue@gmail.com>
2023-10-10 19:17:39 -0700

    test 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,6 +2,6 @@
 
 int main(){
 	char text[100];
-	text = '\"Programming is like building a multilingual puzzle';
+	text = "\"Programming is like building a multilingual puzzle";
 	puts(text);
 	return 0;
