055307d44ecf1b77960a42a265ebe15e7565cf16
Goaty-yagi <tw200blue@gmail.com>
2023-10-10 19:13:01 -0700

    fix 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,6 @@
 #include <stdio.h>
-int main(){puts('\"Programming is like building a multilingual puzzle'); return 0;}
+
+int main(){
+	char text[] = '\"Programming is like building a multilingual puzzle';
+	puts(text);
+	return 0;
