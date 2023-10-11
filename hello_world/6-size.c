74b9abf30e55ba2af0c7d12ee1e2f6a77894dd70
Goaty-yagi <tw200blue@gmail.com>
2023-10-10 22:26:11 -0700

    test 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -15,15 +15,15 @@ int main(void)
 
 	float floatType;
 
-	printf("Size of a char: %zu\n", sizeof(charType));
+	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
 
-	printf("Size of a int: %zu\n", sizeof(intType));
+	printf("Size of a int: %zu byte(s)\n", sizeof(intType));
 
-	printf("Size of a long int: %zu\n", sizeof(intType));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(intType));
 
-	printf("Size of a long long int: %zu\n", sizeof(longLongIntType));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(longLongIntType));
 
-	printf("Size of a float: %zu\n", sizeof(floatType));
+	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 
 	return (0);
 
