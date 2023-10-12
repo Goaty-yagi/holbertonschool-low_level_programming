69d3326d22f010d7aff69656fe1b5e583a98fa6d
Goaty-yagi <tw200blue@gmail.com>
2023-10-12 02:53:49 -0700

    fix 101

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -5,12 +5,12 @@
 /**
  * main - print without printf and other c built-in-functions
  *
- * Return Always 1 (Success)
+ * Return: Always 1 (Success)
  */
 
-int main()
+int main(void)
 {
-	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
 	syscall(SYS_write, 1, msg, sizeof(msg));
 
