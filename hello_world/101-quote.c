32370ae378cd2bd0c7351fafa8f128b2187d2756
Goaty-yagi <tw200blue@gmail.com>
2023-10-11 23:14:35 -0700

    add 101-quote.c

---
+++ hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include <sys/syscall.h>
+
+int main()
+{
+	char msg[] = "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+	syscall(SYS_write, 1, msg, sizeof(msg));
+
+	return (1);
+}
