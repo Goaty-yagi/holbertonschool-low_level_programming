2e89ab766fbfa3032a8c8d2f4a222bbd31812c8a
Goaty-yagi <tw200blue@gmail.com>
2023-10-11 23:17:23 -0700

    update 101

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -3,7 +3,7 @@
 
 int main()
 {
-	char msg[] = "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
 	syscall(SYS_write, 1, msg, sizeof(msg));
 
 	return (1);
