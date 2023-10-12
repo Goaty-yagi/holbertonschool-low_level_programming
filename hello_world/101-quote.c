a7ca659de7d2d529b88301abefdd40025b449b84
Goaty-yagi <tw200blue@gmail.com>
2023-10-12 03:01:25 -0700

    test 101

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,18 +1,17 @@
-#include <unistd.h> 
+#include <unistd.h>
 #include <sys/syscall.h>
 #include<stdio.h>
 
 /**
  * main - print without printf and other c built-in-functions
  *
- * Return: Always 1 (Success)
+ * Return: Always 1 (Error)
  */
-
 int main(void)
 {
-	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char msg[] = 'and that piece of art is useful" - Dora Korpar, 2015-10-19';
 
-	syscall(SYS_write, 0, msg, sizeof(msg));
+	syscall(SYS_write, 1, msg, sizeof(msg));
 
 	return (1);
 
