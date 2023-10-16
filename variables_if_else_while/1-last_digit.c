69f7e633b3bcf6be905a12dc1cfc114cb349d27e
Goaty-yagi <tw200blue@gmail.com>
2023-10-15 18:32:49 -0700

    update file 1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -11,23 +11,27 @@ int main(void)
 {
 	int n, lastDegit;
 
-	char text[] = "Last degit of";
-	
+	char text[] = "Last digit of";
+
+	char gFive[] = "and is greater than 5";
+
+	char lSix[] = "and is less than 6 and not 0"; 
+
 	srand(time(0));
-	
+
 	n = rand() - RAND_MAX / 2;
 
 	lastDegit = n % 10;
 
 	if (n > 5)
 	{
-		printf("%s %d %s %d %s\n", text, n, "is", lastDegit, "and is greater than 5");
+		printf("%s %d %s %d %s\n", text, n, "is", lastDegit, gFive);
 	} else if (n == 0)
 	{
 		printf("%s %d %s %d %s\n", text, n, "is", lastDegit, "and is 0");
 	} else
 	{
-		printf("%s %d %s %d %s\n", text, n, "is", lastDegit, "and is less than 6 and not 0");
+		printf("%s %d %s %d %s\n", text, n, "is", lastDegit, lSix);
 	}
 
 	return (0);
