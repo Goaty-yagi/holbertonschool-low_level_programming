c99ad68d8c75a8881206dedf8426323fc6ae2626
Goaty-yagi <tw200blue@gmail.com>
2023-10-16 05:20:57 -0700

    add 3-islower.c

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _islower - calls _islower
+ *
+ * Return: int 0 or 1
+ */
+
+int _islower(int c)
+{
+	if (islower(c))
+	{
+		return (1)
+	}
+	else
+	{
+		return (0)
+	}
+
+}
