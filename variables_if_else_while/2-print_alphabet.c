54a7496274c47a251ff5ece995a29eab5c8512d4
Goaty-yagi <tw200blue@gmail.com>
2023-10-15 19:17:15 -0700

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Print text
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void) {
+
+	char c;
+
+	for(c = "a", a <='z', ++c) {
+		putchar(c);
+	};
+
+	return (0);
+
+}
