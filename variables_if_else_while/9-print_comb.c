8ebf1ad01b3335c5a96ee7f6d1c9510ce032f112
Goaty-yagi <tw200blue@gmail.com>
2023-10-15 21:46:30 -0700

    add 9-print_comb.c

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Print text
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	int n;
+
+	for (n = 0; n <= 9; ++n)
+	{
+		putchar('0' + n);
+		if (n != 9)
+		{
+			putchar(44)
+		}
+	};
+
+	putchar('\n');
+
+	return (0);
+
+}
