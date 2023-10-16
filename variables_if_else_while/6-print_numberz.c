8500e27bb6e46fdd98ded4612b9e6aaa58b9adeb
Goaty-yagi <tw200blue@gmail.com>
2023-10-15 20:18:21 -0700

    add 6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Print text
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	int n;
+
+	for (n = 0; n <= 9; ++n)
+	{
+		putchar("0" + n);
+	};
+
+	putchar('\n');
+
+	return (0);
+
+}
