99949f1237bbfdf573e8f6850585ed90a999796a
Goaty-yagi <tw200blue@gmail.com>
2023-10-15 23:06:57 -0700

    update file 1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -6,7 +6,7 @@
  * Return: Always 0 (Success)
  */
 
-void print_alphabet(void)
+void print_alphabet()
 {
 	char c;
 
@@ -20,6 +20,6 @@ void print_alphabet(void)
 
 int main(void)
 {
-	print_alphabet()
+	print_alphabet();
 	return (0);
 }
