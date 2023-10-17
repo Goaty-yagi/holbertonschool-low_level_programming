94ebc76ce59ede16b16fb5d71b09b4b9abb6e255
Goaty-yagi <tw200blue@gmail.com>
2023-10-16 19:43:31 -0700

    update file 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -9,6 +9,7 @@
 void times_table(void)
 {
 	int i, n, a;
+
 	for (i = 0; i <= 9; i++)
 	{
 		for (n = 0; n <= 9; n++)
@@ -23,9 +24,18 @@ void times_table(void)
 				_putchar('0' + a / 10);
 				_putchar('0' + a % 10);
 			}
+
 			_putchar(44);
-			_putchar(32);
-			_putchar(32);
+
+			if (a <= 9)
+			{
+				_putchar(32);
+				_putchar(32);
+			}
+			else
+			{
+				_putchar(32);
+			}
 		}
 		_putchar('\n');
 	}
