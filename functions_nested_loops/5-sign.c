65998c9167a0eb0df365bf4e8c3a83139e78d9f7
Goaty-yagi <tw200blue@gmail.com>
2023-10-16 17:14:14 -0700

    update file 5

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -13,19 +13,16 @@ int print_sign(int n)
 	if (n == 0)
 	{
 		_putchar('0' + 0);
-		_putchar(44);
 		return (0);
 	}
 	else if (n > 0)
 	{
 		_putchar(43);
-		_putchar(44);
 		return (1);
 	}
 	else
 	{
 		_putchar(45);
-		_putchar(44);
 		return (-1);
 	}
 }
