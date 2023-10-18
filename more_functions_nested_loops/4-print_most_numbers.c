b09a91060420399ad7e2868f6af37acd71c5d5b2
Goaty-yagi <tw200blue@gmail.com>
2023-10-18 03:03:11 -0700

    add 4-print_most_numbers.c

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - calls print_most_numbers
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int n;
+
+	n = 0;
+
+	while (n <= 9)
+	{
+		if (n != 2 && n != 4)
+		{
+			_putchar('0' + n);
+		}
+	}
+	putchar('\n');
+}
