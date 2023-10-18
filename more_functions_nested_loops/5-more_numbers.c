da15f9cb14d94791a13f79faf3ffd83f73995084
Goaty-yagi <tw200blue@gmail.com>
2023-10-18 03:26:14 -0700

    add 5-more_numbers.c

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * more_numbers - calls more_numbers
+ *
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int n i f;
+
+	n = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		while (n <= 14)
+		{
+			if (n >=10)
+			{
+				f = n / 10;
+			}
+			else
+			{
+				f =  n;
+			}
+			_putchar('0' + f);
+
+			if(n >= 10)
+			{
+				_putchar('0' + n % 10);
+			}
+
+		}
+	_putchar('\n');	
+	}
+}
