7899533c811b0d7c94cae37d9d7190ffd781a608
Goaty-yagi <tw200blue@gmail.com>
2023-10-18 02:52:11 -0700

    add 3-print_numbers.c

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_numbers - calls print_numbers
+ *
+ * Return: check if parameter is lower case
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	n = 0;
+
+	while (0 < 9)
+	{
+		_putchar('0' + n);
+
+		n = n + 1;
+	}
+	_putchar('\n')
+}
