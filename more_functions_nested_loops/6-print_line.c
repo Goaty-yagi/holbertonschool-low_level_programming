157634b2de5fdeb0419e491f297d8619ce89ee55
Goaty-yagi <tw200blue@gmail.com>
2023-10-18 17:04:35 -0700

    add 6-print_line.c

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * print_line - calls print_line
+ * @n: charcter integer from ascii table
+ *
+ * Return: check if parameter is lower case
+ */
+
+void print_line(int n)
+{
+
+	while (n > 0)
+	{
+		_putchar(95;)
+		n = n - 1;
+	}
+	_putchar('\n');
+}
