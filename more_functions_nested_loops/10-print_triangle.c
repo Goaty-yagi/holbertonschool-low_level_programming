da61f3efafcd6341d3f19dcab0c5f44d96fb679d
Goaty-yagi <tw200blue@gmail.com>
2023-10-18 22:36:25 -0700

    add 10-print_triangle.c

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * print_triangle - calls print_triangle
+ * @size: int
+ *
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int i, l, w, m;
+
+	i = w = 0;
+
+	l = 1;
+
+	m = size * size;
+
+	if (size > 0)
+	{
+		while (i <= m)
+		{
+			if (size - w > l)
+			{
+				_putchar(35);
+				w = w + 1;
+			}
+			else
+			{
+				_putchar(32);
+			}
+			i = i + 1;
+			if (i == size * l && i != m)
+			{
+				_putchar('\n');
+				l = l + 1;
+			}
+
+		}
+		_putchar(32);
+	}
+	else
+	{
+		putchar('\n');
+	}
+}
