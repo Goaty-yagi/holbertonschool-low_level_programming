f705e926a4430fd1748ada766e877004962805d8
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-10-23 15:54:16 +1100

    update file 4

--- pointers_arrays_strings/main.c
+++ pointers_arrays_strings/main.c
@@ -2,15 +2,18 @@
 #include <stdio.h>
 
 /**
- * main - check the code 
+ * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-   char *str;
-
-    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+    char *str;
+	int a;
+    str = "Holberton!";
     print_rev(str);
+	a = _strlen(str);
+
+	printf("%d", a);
     return (0);
 }
