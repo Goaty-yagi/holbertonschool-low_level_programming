d6f5f86d22e4472c18d827e6cf5e39f7013e1fb9
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-10-23 17:48:13 +1100

    update file 5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -13,15 +13,15 @@ void rev_string(char *s)
 {
     int n, i;
 
-    char * r_s;
+	n = strlen(s);
 
-	n = strlen(s) - 1;
+    char r_s[n];
 
     i = 0;
 
 	while (n >= 0)
 	{
-        r_s[i] = s[n];
+        r_s[i] = s[n - 1];
  		n = n - 1;
         i = i + 1;
 	}
