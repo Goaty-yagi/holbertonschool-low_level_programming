e648344e0cf44cee89554f368c1a527d32367521
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-10-23 21:59:51 +1100

    update file 7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -4,25 +4,26 @@
 
 /**
  * puts_half - calls puts_half
- * @s: char array
+ * @str: char array
  *
  * Return: void
  */
 
 void puts_half(char *str)
 {
-    int n, h;
-
-    n = 0;
-
-    h = strlen(str);
-
-    while (n < h)
-    {
-        if (n >= h / 2)
-        {
-            _putchar(str[n]);
-        }
-        n = n + 1;
-    }
+	int n, h;
+	
+	n = 0;
+	
+	h = strlen(str);
+	
+	while (n < h)
+	{
+		if (n >= h / 2)
+		{
+			_putchar(str[n]);
+		}
+		n = n + 1;
+	}
+	_putchar('\n');
 }
