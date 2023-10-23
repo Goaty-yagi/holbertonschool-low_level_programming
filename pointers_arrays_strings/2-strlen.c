f71b77336449d1893f93b8d4d319addd50c43b9d
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-10-23 15:00:03 +1100

    init pointers_arrays_strings and add 1 to 4

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * _strlen - calls _strlen
+ * @n: char array
+ *
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+    return strlen(s); // from string.h
+}
---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_rev - calls print_rev
+ * @s: char array
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+    int length, n;
+
+    length = n = strlen(s);
+
+    while(n >= 0)
+    {
+        _putchar(s[n]);
+        n = n - 1;
+    }
+}
