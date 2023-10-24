2107e5067b3935daa3f6a83fa6f336c3a13be443
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-10-24 23:16:53 +1100

    update file 2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -13,13 +13,14 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	if (n != 0) {
+	if (n != 0)
+	{
 
 		char *d = dest;
 		const char *s = src;
-		while (--n != 0) /* --n n - 1 and assign like  n = n - 1 */
+		while (n != 0) /* --n n - 1 and assign like  n = n - 1 */
 		{
-			if ((*d++ = *s++) == 0) 
+			if ((*d++ = *s++) == 0)
 			{
 				while (--n != 0)
 				{
@@ -27,6 +28,7 @@ char *_strncpy(char *dest, char *src, int n)
 				}
 				break;
 			}
+			n = n - 1;
 		}
 	}
 	return (dest);
