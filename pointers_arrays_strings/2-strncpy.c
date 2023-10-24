9dcf41660df03fe2bf8e2843479fe805b37cd095
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-10-24 21:45:36 +1100

    update file 2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
 
 /**
  * _strncpy - calls _strncpy
@@ -7,23 +8,29 @@
  * @src: src char array
  * @n: integer
  *
- * Return:
+ * Return: 
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
 	char *saved;
 
-	int i;
+	int i, length;
+
+	i = 0;
+
+	length = strlen(src);
 
 	saved = dest;
 
-	while (*src&&i < n)
+	while (*src && i < n)
 	{
 		*dest++ = *src++;
 		i = i + 1;
 	}
-	*dest++ = '\0';
-
+	if (n > length)
+	{
+		*dest++ = '\0';
+	}
 	return (saved);
 }
