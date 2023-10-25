037b47d675f69d3e0c393655bb92e3c4f605e275
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-10-25 16:02:55 +1100

    add 5-string_toupper.c and update main.h

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * string_toupper - calls string_toupper
+ * @c: *char
+ *
+ * Return: * char
+ */
+
+char *string_toupper(char *c)
+{
+    char *ch;
+
+    int i;
+
+    i = 0;
+
+    ch = c;
+
+    while (*c)
+    {
+        if(*c >= 97&& *c <= 122)
+        {
+            *c = *c - 32;
+        }
+        c++;
+    }
+
+    return (ch);
+}
