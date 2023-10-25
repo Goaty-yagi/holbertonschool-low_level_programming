f3b12a3aecc84a1882cff8c6e8190d8d5179fa3c
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-10-25 15:48:09 +1100

    add 4-rev_array.c and update main.h

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * reverse_array - calls reverse_array
+ * @a: *int
+ * @n: int
+ *
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+
+    while (0 < n)
+    {
+        printf("%d", a[n-1]);
+        n = n - 1;
+        if(n != 0)
+        {
+            printf(", ");
+        }
+
+    }
+}
