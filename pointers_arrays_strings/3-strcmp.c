34ca3ea3aafc279afbef8cebe7d38cc0de87208c
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-10-25 14:07:34 +1100

    fix for betty

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -12,26 +12,26 @@
 
 int _strcmp(char *s1, char *s2)
 {
-    const char *s_s1, *s_s2;
+	const char *s_s1, *s_s2;
 
-    if (s1 == s2)
-    {
-        return (0);
-    }
+	if (s1 == s2)
+	{
+		return (0);
+	}
 
-    s_s1 = s1;
-    
-    s_s2 = s2;
+	s_s1 = s1;
 
-    while (*s_s2)
-    {
-        if (*s_s1 != *s_s2)
-        {
-            return (*s_s1 - *s_s2);
-        }
-        s_s2++;
-        s_s1++;
-    }
+	s_s2 = s2;
 
-    return (0);
+	while (*s_s2)
+	{
+		if (*s_s1 != *s_s2)
+		{
+			return (*s_s1 - *s_s2);
+		}
+		s_s2++;
+		s_s1++;
+	}
+
+	return (0);
 }
