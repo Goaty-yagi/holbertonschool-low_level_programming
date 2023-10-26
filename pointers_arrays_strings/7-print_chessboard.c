95e84c5f0154240da2be84db80939c0825830eb1
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-10-26 22:42:53 +1100

    add 7-print_chessboard.c and update main.h

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * _strpbrk - calls _strpbrk
+ * @a: nested char pointer
+ *
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+    int a_size, a_counter, b_counter;
+
+    a_size = sizeof(*a);
+	
+    a_counter = b_counter = 0;
+
+    while (b_counter < a_size)
+    {
+        while (a_counter < 8)
+        {
+            printf("%c", a[b_counter][a_counter]);
+            a_counter = a_counter + 1;
+        }
+        printf("\n");
+        b_counter++;
+        a_counter = 0;
+    }
+}
