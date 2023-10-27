e608817d6823fcc3a2a7da7f3c8e8d36a514d8e7
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-10-27 17:59:02 +1100

    update file 8

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -12,32 +12,31 @@
 
 void print_diagsums(int *a, int size)
 {
-    int row = 0;
-    int col = 0;
-    int ltr = 0;
-    int rtl = 0;
-    int index;
+	int row = 0;
+	int col = 0;
+	int ltr = 0;
+	int rtl = 0;
+	int index;
 
-    while (row < size)
-    {
-        while (col < size)
-        {
-            if (row == col)
-            {
-                index = (size * row) + row;
-                ltr = ltr + a[index];
-            }
-            if (col == ((size - 1) - row))
-            {
-                index = (size * row) + ((size - 1) - row);
-                rtl = rtl + a[index];
-            }
-            col = col + 1;
-        }
-        row = row + 1;
-        col = 0;
-    }
-    printf("%d, ", ltr);
-    printf("%d\n", rtl);
-
+	while (row < size)
+	{
+		while (col < size)
+		{
+			if (row == col)
+			{
+				index = (size * row) + row;
+				ltr = ltr + a[index];
+			}
+			if (col == ((size - 1) - row))
+			{
+				index = (size * row) + ((size - 1) - row);
+				rtl = rtl + a[index];
+			}
+			col = col + 1;
+		}
+		row = row + 1;
+		col = 0;
+	}
+	printf("%d, ", ltr);
+	printf("%d\n", rtl);
 }
