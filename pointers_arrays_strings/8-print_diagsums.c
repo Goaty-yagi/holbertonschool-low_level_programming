58dd56ab0ee7840a47285eb1c6fd834c9e5841c0
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-10-27 12:57:10 +1100

    add 8-print_diagsums.c and update main.h

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * print_diagsums - calls print_diagsums
+ * @a: int pointer
+ * @size: int
+ *
+ * Return: void
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int a_counter, e_line, a_size, first_sum, second_sum, f_counter, s_counter;
+
+	int *array = a;
+
+	a_counter = f_counter = e_line = 0;
+
+	s_counter = size - 1;
+
+	a_size = size * size;
+	
+	while (a_counter < a_size)
+	{
+		if (a_counter == f_counter)
+		{
+			first_sum = first_sum + array[a_counter];
+		}
+		if (a_counter == s_counter)
+		{
+			second_sum = second_sum + array[a_counter];
+		}
+		if ((a_counter + 1) % size == 0 && a_counter != 0)
+		{
+			e_line = e_line + 1;
+			f_counter = size * e_line + e_line;
+			s_counter = size * e_line + (size - (e_line + 1));
+		}
+		a_counter = a_counter + 1;
+	}
+	printf("%d, %d", first_sum, second_sum);
+	printf("\n");
+}
