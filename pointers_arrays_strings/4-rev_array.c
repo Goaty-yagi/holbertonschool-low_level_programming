63acaa6d54bbfffe9603cb49a4dd59c5f72060f9
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-10-27 16:01:25 +1100

    update file 4

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -12,13 +12,20 @@
 
 void reverse_array(int *a, int n)
 {
-	while (0 < n)
-	{
-		printf("%d", a[n - 1]);
-		n = n - 1;
-		if (n != 0)
-		{
-			printf(", ");
-		}
-	}
+	int i, len, keep_int;
+
+	i = 0;
+    
+    keep_int = a[0];
+
+    len = n - 1;
+
+    while (i < n / 2)
+    {
+        a[i] = a[len];
+        a[len] = keep_int;
+        len = len - 1;
+        i = i + 1;
+        keep_int = a[i];
+    }
 }
