4e369d99b52eed2e613f4ba05b520c371f19f505
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-10-30 10:59:02 +1100

    add 3-factorial.c and update main.h

---
+++ recursion/3-factorial.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * factorial - calls factorial
+ * @n: int
+ *
+ * Return: int
+ */
+
+int factorial(int n)
+{
+
+    if(n == 0)
+    {
+        return (1);
+    }
+    if (n < 0)
+    {
+        return (-1);
+    }
+
+    return (n * factorial(n - 1));
+}
