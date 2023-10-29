0d6ff21a1ed2f3975b0e1a4d8436df0620ad31c0
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-10-30 10:11:58 +1100

    redursion init include file 0,main.c, main.o, README.md and Makefile

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _puts_recursion - calls _puts_recursion
+ * @s: char array
+ *
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
