957d9b99d88410c17fb8fa8096e5e7a70a8fdc21
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-10-30 10:35:11 +1100

    add 2-strlen_recursion.c and update main.h

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen_recursion - calls _strlen_recursion
+ * @s: char array
+ *
+ * Return: int
+ */
+
+int _strlen_recursion(char *s)
+{
+    return strlen(s);
+}
