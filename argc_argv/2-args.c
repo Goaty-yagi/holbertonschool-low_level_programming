c2ac2fec9e63088d31af26b706bf47fabff3fef6
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-10-30 20:02:13 +1100

    add 2-args.c

---
+++ argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Print text
+ * @argc: int
+ * @argv: char array pointer
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i ;
+
+	i = 0;
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i = i + 1;
+	}
+
+	return (0);
+}
