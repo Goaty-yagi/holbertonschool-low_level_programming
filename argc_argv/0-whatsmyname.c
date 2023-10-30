20d53f4573303aa2a5b65d60ee24b0829965ae71
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-10-30 18:04:38 +1100

    update file 0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,13 +2,15 @@
 
 /**
  * main - Print text
- *
+ * @argc: int
+ * @argv: char array pointer 
+ * 
  * Return: Always 0 (Success)
  */
 
 int main(int argc, char *argv[])
 {
-	printf("%s", argv[argc - 1]);
+	printf("%s\n", argv[argc - 1]);
 
 	return (0);
 }
