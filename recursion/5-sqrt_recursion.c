2fbffdedaaf6551e066fc2e0557fd162b3c9951c
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-10-30 13:06:12 +1100

    add 5-sqrt_recursion.c and add main.h

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+/**
+ * _sqrt_recursion - calls _sqrt_recursion
+ * @n: int
+ *
+ * Return: int
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (sqrt(n));
+}
