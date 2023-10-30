893623f52811f674a2a9d246092ec08732c9bbca
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-10-30 21:36:15 +1100

    update file 4

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -3,28 +3,56 @@
 #include <ctype.h>
 
 /**
+ * check - Print check
+ * @ch: char array
+ *
+ * Return: int
+ */
+int check(char *ch)
+{
+	char *p = ch;
+	int error;
+	error = 0;
+	while (*p)
+	{
+		if (*p >= 48 && *p <= 57)
+		{
+			p++;
+		}
+		else
+		{
+			error = 1;
+			break;
+		}
+	}
+	return (error);
+}
+/**
  * main - Print text
  * @argc: int
  * @argv: char array pointer
  *
  * Return: Always 0 (Success)
  */
+
 int main(int argc, char *argv[])
 {
-	int i, t, result;
+	int i, t, result, error;
 
 	i = 1;
 
-	result = 0;
+	result = error = 0;
+	 
 	if (!argv[1])
 	{
 		i = argc;
 	}
 	while (i < argc)
 	{
-		t = strtol(argv[i], NULL, 10);
-		if (t)
+		error = check(argv[i]);
+		if (!error)
 		{
+			t = strtol(argv[i], NULL, 10);
 			result = result + t;
 		}
 		else
@@ -33,6 +61,7 @@ int main(int argc, char *argv[])
 			return (1);
 		}
 		i = i + 1;
+		error = 0;
 	}
 	printf("%d\n", result);
 	return (0);
