027130c68f34a276232b9e27d4f003272a9e4da6
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-11-06 17:04:51 +1100

    add 2-str_concat.c

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * str_concat - calls str_concat
+ * @s1:  str pointer
+ * @s2:  str pointer
+ *
+ * Return: concatenates s1 and s2 then retuen it as a new pointer
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *new_char;
+
+    int len, counter;
+
+    len = strlen(s1) + strlen(s2);
+
+    counter = 0;
+
+    new_char = malloc(len * sizeof(char));
+
+    if (new_char == NULL)
+    {
+        return (NULL);
+    }
+
+    new_char[len] = '\0';
+
+    while (counter < len)
+    {
+        if (*s1)
+        {
+            new_char[counter] = *s1;
+            s1++;
+        }
+        else
+        {
+            new_char[counter] = *s2;
+            s2++;
+        }
+        counter = counter + 1;
+    }
+    return (new_char);
+
+}
