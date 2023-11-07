fa4656cbcc5250481a92841a3a13891673e4eb9a
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-11-07 17:25:55 +1100

    add 2-main.c

---
+++ preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _puts_recursion - calls _puts_recursion
+ * @s: char array
+ *
+ * Return: void
+ */
+
+int main(void)
+{
+	printf("%s", __FILE__);
+    return (0);
+}
