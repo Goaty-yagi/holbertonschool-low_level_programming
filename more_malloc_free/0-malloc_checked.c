406a6962dfa3690b057427786fa72f0b0be54b80
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-11-07 11:12:02 +1100

    init more_malloc_free

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - calls malloc_checked
+ * @b:  int
+ *
+ * Return: char array
+ */
+
+void *malloc_checked(unsigned int b)
+{
+}
