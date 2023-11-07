68f3a5f2cae7b699e8dc93e232c34cc133a51a9c
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-11-07 13:11:40 +1100

    add 1-string_nconcat.c

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,56 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * string_nconcat - calls string_nconcat
+ * @s1:  char
+ * @s2:  char
+ * n:  int
+ *
+ * Return: pointer
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *new_char;
+
+	int len_1, len_2, counter;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	counter = 0;
+
+	len_1 = strlen(s1);
+
+	if (strlen(s2) <= n)
+	{
+		len_2 = strlen(s2);
+	}
+	else
+	{
+		len_2 = n;
+	}
+
+	new_char = malloc(len_1 + len_2 * sizeof(char) + 1);
+	if (new_char == NULL)
+		return (NULL);
+
+	while (counter < len_2 + len_1)
+	{
+		if (*s1)
+		{
+			new_char[counter] = *s1++;
+		}
+		else
+		{
+			new_char[counter] = *s2++;
+		}
+		counter = counter + 1;
+	}
+	return (new_char);
+}
