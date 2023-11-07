907c9ccc1c749f5a38b71cb1c843633d3d6ed9a2
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-11-07 15:36:16 +1100

    add 3-array_range.c

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * array_range - calls array_range
+ * @min:  int
+ * @max:  int
+ *
+ * Return: pointer
+ */
+
+int *array_range(int min, int max)
+{
+	int *array, counter;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+	array = malloc((max - min) + 1);
+
+	counter = 0;
+
+	while (min <= max)
+	{
+		array[counter] = min;
+		min = min + 1;
+		counter = counter + 1;
+	}
+	return (array);
+}
