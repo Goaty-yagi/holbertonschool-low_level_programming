b8b850ef4c4b00e5df92333cc780a95618fc7538
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-11-13 12:46:45 +1100

    update dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,10 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-struct dog {
-   char *name;
-   float age;
-   char *owner;
+/**
+ * struct dog - dog type
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: a dog info type for dog obj
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
 };
 
 #endif
