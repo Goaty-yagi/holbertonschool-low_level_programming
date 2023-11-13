b8ecd4babad580cd7154c9d4097ffb7133faf516
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-11-13 13:34:35 +1100

    update file 2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -4,14 +4,28 @@
 /**
  * print_dog - calls print_dog
  * @d: pointer
-
  *
  * Return: void
  */
 
 void print_dog(struct dog *d)
 {
-	printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	char *nil;
+
+	nil = "(nil)";
+
+	if (d)
+	{
+		printf("Name: %s\n", d->name ? d->name : nil);
+		
+		if (d->age)
+		{
+			printf("Age: %f\n", d->age);
+		}
+		else
+		{
+			printf("Age: %s\n", nil);
+		}
+		printf("Owner: %s\n", d->owner ? d->owner : nil);
+	}
 }
