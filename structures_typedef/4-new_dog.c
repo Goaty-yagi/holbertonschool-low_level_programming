ab5e7cc654bfe7ceb835695a06b901c24db5b8e2
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-11-13 16:59:04 +1100

    update file 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -14,21 +14,20 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t d;
 
 	dog_t *new_d;
-
-	if (name == NULL || owner == NULL)
+	new_d = malloc(sizeof(dog_t));
+	if (new_d == NULL)
 	{
 		return (NULL);
 	}
-	new_d = &d;
 	new_d->name = strdup(name);
 	new_d->owner = strdup(owner);
 	if (new_d->name == NULL || new_d->owner == NULL)
 	{
 		free(new_d->name);
 		free(new_d->owner);
+		free(new_d);
 		return (NULL);
 	}
 	new_d->age = age;
