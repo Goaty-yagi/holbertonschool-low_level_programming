ad1a749768341c275d633dc008f0f12385daa783
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-11-13 23:06:33 +1100

    update file 1

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -4,7 +4,6 @@
 /**
  * array_iterator - calls array_iterator
  * @array: int array
- * @size_t: size of array
  * @action: function
  *
  * Return: void
@@ -12,13 +11,16 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-    unsigned int counter;
+	unsigned int counter;
 
-    counter = 0;
+	if (array && action)
+	{
+		counter = 0;
 
-    while (counter < size)
-    {
-        action(array[counter]);
-        counter = counter + 1;
-    }
+		while (counter < size)
+		{
+			action(array[counter]);
+			counter = counter + 1;
+		}
+	}
 }
