ed22fa1f91392c71aae3a77598a02c811d0479d8
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-11-13 17:22:04 +1100

    add 5-free_dog.c

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * free_dog - calls free_dog
+ * @d: struct pointer
+ *
+ * Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d);
+}
