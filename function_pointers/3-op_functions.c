6d37a8f52045f300c38b7aeda9ec72d62a63b08d
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-11-14 17:34:59 +1100

    update 3 files

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -7,58 +7,52 @@
  * @a: num1
  * @b: numb
  *
- * Context:  sum of a and b
+ * Description:  sum of a and b
  * Return:  returns the sum of a and b
- */
-
-/**
- * op_sub() - calls op_sub
- * @a: num1
- * @b: numb
- *
- * Context:  difference of a and b
- * Return:  returns the difference of a and b
- */
-
-/**
- * op_mul() - calls op_mul
- * @a: num1
- * @b: numb
- *
- * Context: product of a and b
- * Return:  returns the product of a and b
- */
-
-/**
- * op_div() - calls op_div
- * @a: num1
- * @b: numb
- *
- * Context: division of a by b
- * Return:  returns the result of the division of a and b
- */
-
-/**
- * op_mod() - calls op_mod
- * @a: num1
- * @b: numb
- *
- * Context: division of a by b
- * Return:  returns the remainder of the division of a by b
  */
 
 int op_add(int a, int b)
 {
 	return (a + b);
 }
+
+/**
+ * op_sub() - calls op_sub
+ * @a: num1
+ * @b: numb
+ *
+ * Description:  difference of a and b
+ * Return:  returns the difference of a and b
+ */
+
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+
+/**
+ * op_mul() - calls op_mul
+ * @a: num1
+ * @b: numb
+ *
+ * Description: product of a and b
+ * Return:  returns the product of a and b
+ */
+
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
+
+/**
+ * op_div() - calls op_div
+ * @a: num1
+ * @b: numb
+ *
+ * Description: division of a by b
+ * Return:  returns the result of the division of a and b
+ */
+
 int op_div(int a, int b)
 {
 	if (b == 0)
@@ -68,6 +62,16 @@ int op_div(int a, int b)
 	}
 	return (a / b);
 }
+
+/**
+ * op_mod() - calls op_mod
+ * @a: num1
+ * @b: numb
+ *
+ * Description: division of a by b
+ * Return:  returns the remainder of the division of a by b
+ */
+
 int op_mod(int a, int b)
 {
 	if (b == 0)
