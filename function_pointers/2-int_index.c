ffcdcf153744173be97b54a49e51c6466b0e5abe
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-11-14 12:40:35 +1100

    add 2-int_index.c update header

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,32 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - calls int_index
+ * @array: int array
+ * @size: array size
+ * @cmp: function
+ *
+ * Return: void
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int result, counter;
+
+	if(!size)
+	{
+		return (-1);
+	}
+	counter = 0;
+
+	while (counter < size)
+	{
+		result = cmp(array[counter]);
+		if (result)
+		{
+			return (counter);
+		}
+		counter = counter + 1;
+	}
+	return (-1);
+}
