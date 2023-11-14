ac046a8d9f26e680c9b73c42451d273f3af80904
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-11-14 23:03:17 +1100

    add 3-print_all.c update variadic_functions.h

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,63 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_all - calls print_all
+ * @*: string
+ * @format: int
+ * @...: rest
+ *
+ * Return: void
+ */
+
+void print_all(const char *const format, ...)
+{
+    va_list ap;
+
+    char *result;
+
+    unsigned int i, c;
+
+    i = c = 0;
+    va_start(ap, format);
+    while (format[i])
+    {
+        switch (format[i])
+        {
+        case 'c':
+            printf("%c", va_arg(ap, int));
+			c++;
+            break;
+        case 'i':
+            printf("%d", va_arg(ap, int));
+			c++;
+            break;
+        case 'f':
+            printf("%f", va_arg(ap, double));
+			c++;
+            break;
+        case 's':
+            result = va_arg(ap, char *);
+            if (result != NULL)
+            {
+                printf("%s", result);
+            }
+            else
+            {
+                printf("%s", result);
+            }
+			c++;
+            break;
+        }
+        i = i + 1;
+        if (format[i] && c)
+        {
+            printf(", ");
+        }
+		c = 0;
+    }
+    printf("\n");
+    va_end(ap);
+}
