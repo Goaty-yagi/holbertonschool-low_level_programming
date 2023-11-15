aadf21a19ef1ad273ee5c17226cb978f8e8bbb37
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-11-15 14:10:57 +1100

    update 3-print_all.c

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,39 +1,50 @@
 #include "variadic_functions.h"
 /**
  * print_all - calls print_all
- * @format: char
+ * @format: int
  * @...: rest
  * Return: void
  */
 void print_all(const char *const format, ...)
 {
-	va_list ap; char *result, null[6] = "(nil)"; unsigned int i, c; va_start(ap, format);
-	if (format == NULL)
-		return;
-	i = c = 0;
-	while (format[i])
+	va_list ap;
+
+	char *result;
+
+	char null[6] = "(nil)";
+
+	unsigned int i, c;
+
+	va_start(ap, format);
+	if (format != NULL)
 	{
-		switch (format[i])
+		i = c = 0;
+		while (format[i])
 		{
-		case 'c':
-			printf("%c", va_arg(ap, int));
-			break;
-		case 'i':
-			printf("%d", va_arg(ap, int));
-			break;
-		case 'f':
-			printf("%f", va_arg(ap, double));
-			break;
-		case 's':
-			result = va_arg(ap, char *);
-			printf("%s", result != NULL ? result : null);
-			break;
-		default:
-			c = 1;
+			switch (format[i])
+			{
+			case 'c':
+				printf("%c", va_arg(ap, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(ap, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(ap, double));
+				break;
+			case 's':
+				result = va_arg(ap, char *);
+				printf("%s", result != NULL ? result : null);
+				break;
+			default:
+				c = 1;
+			}
+			i = i + 1;
+			if (format[i] && !c)
+				printf(", ");
+			c = 0;
 		}
-		if (format[i] && !c)
-			printf(", ");
-		c = 0; i = i + 1;
 	}
-	printf("\n");va_end(ap);
+	printf("\n");
+	va_end(ap);
 }
