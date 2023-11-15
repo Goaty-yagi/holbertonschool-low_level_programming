bb6c135853ef74151d66555b5d95f1721a44dc6b
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-11-15 14:02:43 +1100

    update 3-print_all.c variadic_functions.h

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,55 +1,38 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
-#include <stdio.h>
 /**
  * print_all - calls print_all
- * @*: string
- * @format: int
+ * @format: char
  * @...: rest
  * Return: void
  */
 void print_all(const char *const format, ...)
 {
-	va_list ap;
-
-	char *result;
-
-	char null[6] = "(nil)";
-
-	unsigned int i, c;
-
-	va_start(ap, format);
-	if (format != NULL)
+	va_list ap; char *result, null[6] = "(nil)"; unsigned int i, c; va_start(ap, format);
+	if (format == NULL)
+		return;
+	for (i = 0; format[i]; i++)
 	{
-		i = c = 0;
-		while (format[i])
+		switch (format[i])
 		{
-			switch (format[i])
-			{
-			case 'c':
-				printf("%c", va_arg(ap, int));
-				c++;
-				break;
-			case 'i':
-				printf("%d", va_arg(ap, int));
-				c++;
-				break;
-			case 'f':
-				printf("%f", va_arg(ap, double));
-				c++;
-				break;
-			case 's':
-				result = va_arg(ap, char *);
-				printf("%s", result != NULL ? result : null);
-				c++;
-				break;
-			}
-			i = i + 1;
-			if (format[i] && c)
-				printf(", ");
-			c = 0;
+		case 'c':
+			printf("%c", va_arg(ap, int));
+			break;
+		case 'i':
+			printf("%d", va_arg(ap, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(ap, double));
+			break;
+		case 's':
+			result = va_arg(ap, char *);
+			printf("%s", result != NULL ? result : null);
+			break;
+		default:
+			c = 1;
 		}
+		if (format[i + 1] && !c)
+			printf(", ");
+		c = 0;
 	}
-	printf("\n");
-	va_end(ap);
+	printf("\n");va_end(ap);
 }
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,5 +1,7 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
+#include <stdarg.h>
+#include <stdio.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
