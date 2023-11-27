4f4ed021ab005ae654c187b1bbff33eccdd0dce2
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-11-27 22:46:10 +1100

    update 4-free_list.c

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -10,10 +10,12 @@
 void free_list(list_t *head)
 {
 	list_t *temp;
+
 	while (head != NULL)
 	{
-		temp = head->next;
+		temp = head;
 		head = head->next;
+		free(temp->str);
 		free(temp);
 	}
 }
