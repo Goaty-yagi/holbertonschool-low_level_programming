b749afb283511e5f20a603f84aca3aa95417a9dc
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-11-29 21:15:49 +1100

    add 8-delete_dnodeint.c update lists.h

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,50 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - calls delete_dnodeint_at_index
+ * @head: node header pointer pointer
+ * @index: int
+ *
+ * Return: int
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *p, *pre;
+
+	unsigned int counter;
+	p = *head;
+	if (*head == NULL)
+	{
+		return (-1);
+	}
+	counter = 0;
+	if (index == 0)
+	{
+		p->next->prev = NULL;
+		free(p);
+		return (1);
+	}
+	while (p != NULL && counter != index)
+	{
+		pre = p;
+		p = p->next;
+		counter = counter + 1;
+	}
+	if (counter == index)
+	{
+		if (p)
+		{
+			p->next->prev = p->prev;
+			p->prev->next = p->next;
+			free(p);
+		}
+		else
+		{
+			pre->prev->next = NULL;
+			free(pre);
+		}
+		return (1);
+	}
+	return (-1);
+}
--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 /**
  * struct dlistint_s - doubly linked list
@@ -28,5 +29,6 @@ void free_dlistint(dlistint_t *head);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
 #endif
