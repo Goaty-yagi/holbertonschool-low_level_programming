f304610f6cf32eff8fd18a8318287ffaaba02565
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-11-29 20:32:36 +1100

    update 7-insert_dnodeint.c

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -13,44 +13,35 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int counter;
 
 	new = malloc(sizeof(dlistint_t));
-	pre = malloc(sizeof(dlistint_t));
-	if (new == NULL || pre == NULL)
+	if (new == NULL)
 		return (NULL);
 	p = *h;
 	counter = 0;
 	new->n = n;
-	new->next = NULL;
-	new->prev = NULL;
+	new->next = new->prev = NULL;
 	if (idx == 0 || *h == NULL)
 	{
 		new->next = *h;
-		if(*h)
+		if (*h)
 			(*h)->prev = new;
 		*h = new;
 		return (new);
 	}
-	while (p != NULL)
+	while (p != NULL && counter != idx)
 	{
-		if (counter == idx)
-		{
-			new->next = p;
-			p->prev = new;
-			new->prev = pre;
-			pre->next = new;
-			if (idx == 0)
-			{
-				*h = new;
-			}
-			return (new);
-		}
 		pre = p;
 		p = p->next;
 		counter = counter + 1;
 	}
 	if (counter == idx)
 	{
+		new->next = p;
 		new->prev = pre;
-		pre->next = new;
+		if (p)
+		{
+			p->prev = new;
+			pre->next = new;
+		}
 		return (new);
 	}
 	return (NULL);
