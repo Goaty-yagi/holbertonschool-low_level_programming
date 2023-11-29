a2fbb9aed5235d3358b80143d95193099de9955a
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-11-29 14:40:51 +1100

    add 5-get_dnodeint.c update lists.h

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - calls get_dnodeint_at_index
+ * @head: node header pointer pointer
+ * @index:  int
+ *
+ * Return: new address
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *p;
+	unsigned int counter;
+
+	p = head;
+	counter = 0;
+
+	while (p->next != NULL)
+	{
+		if (counter == index)
+		{
+			return (p);
+		}
+		p = p->next;
+		counter = counter + 1;
+	}
+	return (NULL);
+}
