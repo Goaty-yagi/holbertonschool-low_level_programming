c6b51ecf27aa60e04dbc969db550c061b859a868
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-11-30 16:25:44 +1100

    update 1-print_binary.c

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -17,7 +17,8 @@ void print_binary(unsigned long int n)
 
 	while (i >= 0)
 	{
-		mask = (unsigned long int)1 << i; /* Set a mask for each bit position. assume mask is 1 concat 0 * n*/
+		mask = (unsigned long int)1 << i; 
+		/* Set a mask for each bit position. assume mask is 1 concat 0 * n*/
 		if (n & mask)
 		{				 /* ex) 1100010 & 1000000 will be 1000000 so true */
 			started = 1; /* Turn on the flag once non-zero bit is found */
