d701a97cd5e009a19cb3b44ded37756672fb1f52
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-11-30 20:55:57 +1100

    update 2-get_bit.c

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -17,15 +17,15 @@ int get_bit(unsigned long int n, unsigned int index)
 
 	if (index >= sizeof(unsigned long int) * 8)
 	{
-		return -1;
+		return (-1);
 	}
 
 	if (n & mask)
 	{
-		return 1;
+		return (1);
 	}
 	else
 	{
-		return 0;
+		return (0);
 	}
 }
