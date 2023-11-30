0f85621510853f510690f1713ad6c698798847c3
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-11-30 20:54:00 +1100

    update 2-get_bit.c

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -10,40 +10,21 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int bitCount, i, started, cp_n;
-	unsigned long int mask, ui, len_b;
+	unsigned long int mask, i;
+	
+	mask = i << index;
 
-	started = 0;
-	bitCount = sizeof(unsigned long int) * 8; /* max 64 bit*/
-	i = bitCount - 1;
-	ui = 1;
-	len_b = 0;
-	cp_n = n;
-	while (cp_n > 0)
+	if (index >= sizeof(unsigned long int) * 8)
 	{
-		cp_n = cp_n / 2;
-		len_b = len_b + 1;
+		return -1;
 	}
-	if (len_b < index && ULONG_MAX <= n)
+
+	if (n & mask)
 	{
-		return (-1);
+		return 1;
 	}
-	while (i >= 0)
+	else
 	{
-		mask = ui << index;
-		if (n & mask)
-		{
-			return (1);
-		}
-		else if (started)
-		{
-			return (0);
-		}
-		i = i - 1;
+		return 0;
 	}
-	if (!started)
-	{
-		return (0);
-	}
-	return (-1);
 }
