4edb2cd69cc9a4a297b945a5f9b6d0b5fbc256f3
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-11-30 21:59:27 +1100

    update 5-flip_bits.c

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -5,7 +5,7 @@
  * @n: int
  * @m: int
  *
- * Return: number of bits you would need to flip to get from one number to another.
+ * Return: number of bits need to flip.
  */
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
