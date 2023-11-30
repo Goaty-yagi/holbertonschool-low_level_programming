9c9f86d2d5aea5729e2efd59c0bd89a3b192c8b9
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-11-30 21:58:20 +1100

    add 5-flip_bits.c update main.h

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <limits.h>
+/**
+ * flip_bits - calls flip_bits
+ * @n: int
+ * @m: int
+ *
+ * Return: number of bits you would need to flip to get from one number to another.
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int bitCount, i;
+	unsigned long int mask, ui, sum;
+
+	sum = 0;
+	bitCount = sizeof(unsigned long int) * 8; /* max 64 bit*/
+	i = bitCount - 1;
+	ui = 1;
+
+	while (i >= 0)
+	{
+		mask = ui << i;
+		if ((n & mask) != (m & mask))
+		{
+			sum = sum + 1;
+		}
+		i = i - 1;
+	}
+	return (sum);
+}
