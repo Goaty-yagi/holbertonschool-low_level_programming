4978e73a3077dff0994c20565243564f7113139c
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-12-04 23:01:24 +1100

    update 3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,22 +1,14 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
-
 /**
  * main - check the code
  * @ac: ac
  * @av: av
- * 
+ *
  * Return: Always 0.
  */
 int main(int ac, char **av)
 {
-	ssize_t bytes_read, bytes_written;
-	int BUFFER_SIZE = 1024;
-	int fd_from;
-	int fd_to;
-	char *file_from;
-	char *file_to;
+	int fd_from, fd_to, bytes_read,BUFFER_SIZE = 1024;
 	char *buffer;
 
 	if (ac != 3)
@@ -24,50 +16,35 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
-	file_from = av[1];
-	file_to = av[2];
 	buffer = malloc(BUFFER_SIZE);
 	if (!buffer)
-	{
 		return (1);
-	}
-	fd_from = open(file_from, O_RDONLY);
+	fd_from = open(av[1], O_RDONLY);
 	if (fd_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-
-	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
-	bytes_read = read(fd_from, buffer, BUFFER_SIZE);
-	while (bytes_read > 0)
+	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
-		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written != bytes_read)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-			exit(99);
-		}
+		write(fd_to, buffer, bytes_read);
 		bytes_read = read(fd_from, buffer, BUFFER_SIZE);
 	}
-
 	if (bytes_read == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-
 	if (close(fd_from) == -1 || close(fd_to) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd ");
 		exit(100);
 	}
-
 	return 0;
 }
