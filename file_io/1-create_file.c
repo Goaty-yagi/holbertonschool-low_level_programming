02870ecfc68b2a7ea4cdef6f8c5b9a3cf4da88b3
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-12-04 15:19:26 +1100

    update 1-create_file.c

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	char *c;
 
 	len = strlen(text_content);
-	c = malloc(sizeof(char) * len + 1);
+	c = malloc(len + 1);
 	c[len] = '\0';
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (!c || !filename)
