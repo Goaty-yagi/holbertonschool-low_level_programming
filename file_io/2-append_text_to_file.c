a2c19831ee2c6bcda1f758b98adf01a619fb4bf1
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-12-04 16:06:18 +1100

    update 2-append_text_to_file.c

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -16,11 +16,10 @@ int append_text_to_file(const char *filename, char *text_content)
 	ssize_t bytes_written;
 	char *c;
 
-	printf("IN FUNC");
 	len = text_content ? strlen(text_content) : 0;
 	c = malloc(len + 1);
 	c[len] = '\0';
-	fd = open(filename, O_RDWR | O_APPEND , S_IRUSR | S_IWUSR);
+	fd = open(filename, O_RDWR | O_APPEND, S_IRUSR | S_IWUSR);
 	if (!c || !filename)
 	{
 		return (-1);
