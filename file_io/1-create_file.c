6bca30b2c92f5508f990cf99f32fd203893023e3
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-12-04 15:01:51 +1100

    update 1-create_file.c

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -13,26 +13,26 @@
 
 int create_file(const char *filename, char *text_content)
 {
-    int len, fd;
-    ssize_t bytes_written;
-    char *c;
+	int len, fd;
+	ssize_t bytes_written;
+	char *c;
 
-    len = strlen(text_content);
-    c = malloc(sizeof(char) * len + 1);
-    c[len] = '\0';
-    fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-    if (!c || !filename)
-    {
-        return (-1);
-    }
-    bytes_written = write(1, text_content, len);
-    if (bytes_written == -1)
-    {
-        close(fd);
-        free(c);
-        return (-1);
-    }
-    close(fd);
-    free(c);
-    return (1);
+	len = strlen(text_content);
+	c = malloc(sizeof(char) * len + 1);
+	c[len] = '\0';
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (!c || !filename)
+	{
+		return (-1);
+	}
+	bytes_written = write(fd, text_content, len);
+	if (bytes_written == -1)
+	{
+		close(fd);
+		free(c);
+		return (-1);
+	}
+	close(fd);
+	free(c);
+	return (1);
 }
