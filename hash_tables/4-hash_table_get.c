886c1478c69409d8878a4ae0e0fa4f95547f4168
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-12-05 20:24:00 +1100

    add 4-hash_table_get.c update hash_tables.h

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - call hash_table_get
+ * @ht: hash_table_t
+ * @key: key
+ *
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int counter;
+
+	counter = 0;
+	(void)key;
+	while (counter < ht->size)
+	{
+		if (ht->array[counter])
+		{
+			if (strcmp(ht->array[counter]->key, key) == 0)
+			{
+				return (ht->array[counter]->value);
+			}
+			if (ht->array[counter]->next)
+			{
+				if (strcmp(ht->array[counter]->next->key, key) == 0)
+				{
+					return (ht->array[counter]->value);
+				}
+			}
+		}
+		counter = counter + 1;
+	}
+	return (NULL);
+}
