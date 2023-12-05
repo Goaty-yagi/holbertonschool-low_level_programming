99ed5acf1bedcb9c20d4ac078ca6bbbdc3ee7371
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-12-05 22:15:42 +1100

    update 3-hash_table_set.c

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -30,6 +30,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		if (strcmp(ht->array[index]->key, node->key) == 0)
 		{
+			free(ht->array[index]);
 			ht->array[index] = node;
 		}
 		else
