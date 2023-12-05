bf123210b5b7a7ec4a00d292fd81aa186f3a860d
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-12-05 11:21:51 +1100

    add 0-hash_table_create.c hash_tables.h

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,16 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - hash_table_create
+ * @size: size of the array
+ *
+ * Return: a pointer to the newly created hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+
+	ht = malloc(sizeof(hash_table_t) * size);
+	return (ht);
+}
