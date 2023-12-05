2583dfde50ae78e11f783c798a36cd5cc2ad4b08
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-12-05 21:17:58 +1100

    update 6-hash_table_delete.c

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -23,10 +23,11 @@ void hash_table_delete(hash_table_t *ht)
 				{
 					free(ht->array[counter]->next);
 				}
+				free(ht->array[counter]);
 			}
-			free(ht->array[counter]);
 			counter = counter + 1;
 		}
+		free(ht->array);
 		free(ht);
 	}
 }
